c7a453817745694075dbf8004f6ce1145780d576
niwamei <niwamei@outlook.com>
2021-04-08 21:17:15 +0900

    チャットルーム作成機能の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,5 +1,21 @@
 class RoomsController < ApplicationController
+  
   def new
     @room = Room.new
+  end
+
+  def create
+    @room = Room.new(rooms_params)
+    if @room.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def rooms_params
+    params.require(:room).permit(:name, user_ids: [])
   end
 end
